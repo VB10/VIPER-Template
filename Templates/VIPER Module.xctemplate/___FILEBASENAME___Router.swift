89832ae0a6ff8a869b437ffb6c79c21fde4557e5
vb10 <vbacik.10@gmail.com>
2024-03-04 01:08:35 +0300

    Update createModule() return type to ___VARIABLE_ModuleName___ViewController

--- Templates/VIPER Module.xctemplate/___FILEBASENAME___Router.swift
+++ Templates/VIPER Module.xctemplate/___FILEBASENAME___Router.swift
@@ -12,7 +12,7 @@ import UIKit
 final class  ___VARIABLE_ModuleName___Router: PresenterToRouter___VARIABLE_ModuleName___Protocol {
     // MARK: Static methods
 
-    static func createModule() -> UIViewController {
+    static func createModule() -> ___VARIABLE_ModuleName___ViewController {
         let viewController = ___VARIABLE_ModuleName___ViewController()
         let interactor = ___VARIABLE_ModuleName___Interactor()
         let router = ___VARIABLE_ModuleName___Router(navigation: viewController)
